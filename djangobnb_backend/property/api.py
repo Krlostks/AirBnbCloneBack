35333ea31b13974e1f2de67695b440c1d61c60c6
Krlostks <carlospixel13@gmail.com>
2025-03-25 10:04:05 -0600

    properties for the app

---
+++ djangobnb_backend/property/api.py
@@ -0,0 +1,19 @@
+from django.http import JsonResponse
+from rest_framework.decorators import api_view, authentication_classes, permission_classes
+from .models import Property
+from .serializers import PropertiesDetailSerializer
+ 
+@api_view(['GET'])
+@authentication_classes([]) 
+@permission_classes([]) 
+def properties_detail(request, property_id):
+     try:
+         property = Property.objects.get(id=property_id)  
+     except Property.DoesNotExist:
+         return JsonResponse({'error': 'Property not found'}, status=404)
+ 
+     serializer = PropertiesDetailSerializer(property)  
+ 
+     return JsonResponse({
+         'data': serializer.data
+     })
---
+++ djangobnb_backend/property/serializers.py
@@ -0,0 +1,33 @@
+from rest_framework import serializers
+ 
+from .models import Property, Reservation
+ 
+ 
+ 
+ 
+class PropertiesListSerializer(serializers.ModelSerializer):
+     class Meta:
+         model = Property
+         fields = (
+             'id',
+             'title',
+             'price_per_night',
+             'image_url',
+         )
+ 
+ 
+class PropertiesDetailSerializer(serializers.ModelSerializer):
+     class Meta:
+         model = Property
+         fields = (
+             'id',
+             'title',
+             'description',
+             'price_per_night',
+             'image_url',
+             'bedrooms',
+             'bathrooms',
+             'guests',
+             'landlord'
+         )
+ 
