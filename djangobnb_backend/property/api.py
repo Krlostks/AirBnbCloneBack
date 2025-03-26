d8e09cb828d9453422b1749055cdcc3ca1b7fbc8
Krlostks <carlospixel13@gmail.com>
2025-03-25 18:13:21 -0600

    Properties for the app

--- djangobnb_backend/property/api.py
+++ djangobnb_backend/property/api.py
@@ -4,16 +4,24 @@ from .models import Property
 from .serializers import PropertiesDetailSerializer
  
 @api_view(['GET'])
-@authentication_classes([]) 
-@permission_classes([]) 
+@authentication_classes([])  
+@permission_classes([])  
 def properties_detail(request, property_id):
      try:
          property = Property.objects.get(id=property_id)  
      except Property.DoesNotExist:
          return JsonResponse({'error': 'Property not found'}, status=404)
  
-     serializer = PropertiesDetailSerializer(property)  
+     serializer = PropertiesDetailSerializer(property) 
  
      return JsonResponse({
          'data': serializer.data
      })
+
+@api_view(['GET'])
+@authentication_classes([]) 
+@permission_classes([])  
+def properties_list(request):
+    properties = Property.objects.all()  
+    serializer = PropertiesDetailSerializer(properties, many=True)  
+    return JsonResponse({'data': serializer.data})
--- djangobnb_backend/property/urls.py
+++ djangobnb_backend/property/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import api
- 
+
 urlpatterns = [
-     path('', api.properties_detail, name='properties_detail'),
+    path('', api.properties_list, name='properties_list'),  
+    path('<uuid:property_id>/', api.properties_detail, name='properties_detail'),  
 ]
