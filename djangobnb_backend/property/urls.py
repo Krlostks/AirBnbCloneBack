ddf9d5a79f3fa7dd5776c71324ddbe07c8122611
Krlostks <carlospixel13@gmail.com>
2025-03-25 21:01:45 -0600

    Add properties from nextjs

--- djangobnb_backend/property/urls.py
+++ djangobnb_backend/property/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import api
 
 urlpatterns = [
-    path('', api.properties_list, name='properties_list'),  
+    path('', api.properties_list, name='properties_list'),
+    path('create/', api.create_property, name='api_create_property'),  
     path('<uuid:property_id>/', api.properties_detail, name='properties_detail'),  
 ]
