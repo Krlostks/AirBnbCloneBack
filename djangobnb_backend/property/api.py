edda480e68cb3796f00a5cc5b6efaac7b2380fae
Krlostks <carlospixel13@gmail.com>
2025-03-26 10:47:13 -0600

    book property

--- djangobnb_backend/property/api.py
+++ djangobnb_backend/property/api.py
@@ -1,9 +1,22 @@
 from django.http import JsonResponse
 from rest_framework.decorators import api_view, authentication_classes, permission_classes
-from .models import Property
-from .serializers import PropertiesDetailSerializer
+
 from .forms import PropertyForm
+from .models import Property , Reservation
+from .serializers import PropertiesListSerializer, PropertiesDetailSerializer, ReservationsListSerializer  
  
+
+@api_view(['GET'])
+@authentication_classes([])  
+@permission_classes([])  
+def properties_list(request):
+     properties = Property.objects.all()  
+     serializer = PropertiesListSerializer(properties, many=True)  
+ 
+     return JsonResponse({
+         'data': serializer.data
+     })
+
 @api_view(['GET'])
 @authentication_classes([])
 @permission_classes([])
@@ -15,12 +28,14 @@ def properties_detail(request, pk):
      return JsonResponse(serializer.data)
 
 @api_view(['GET'])
-@authentication_classes([]) 
-@permission_classes([])  
-def properties_list(request):
-    properties = Property.objects.all()  
-    serializer = PropertiesDetailSerializer(properties, many=True)  
-    return JsonResponse({'data': serializer.data})
+@authentication_classes([])
+@permission_classes([])
+def property_reservations(request, pk):
+     property = Property.objects.get(pk=pk)
+     reservations = property.reservations.all() 
+     serializer = ReservationsListSerializer(reservations, many=True)
+ 
+     return JsonResponse(serializer.data, safe=False)    
 
 @api_view(['POST', 'FILES'])
 def create_property(request):
@@ -35,3 +50,30 @@ def create_property(request):
      else:
          print('error', form.errors, form.non_field_errors)
          return JsonResponse({'errors': form.errors.as_json()}, status=400)
+
+@api_view(['POST'])
+def book_property(request, pk):
+     try:
+         start_date = request.POST.get('start_date', '')
+         end_date = request.POST.get('end_date', '')
+         number_of_nights = request.POST.get('number_of_nights', '')
+         total_price = request.POST.get('total_price', '')
+         guests = request.POST.get('guests', '')
+ 
+         property = Property.objects.get(pk=pk)
+ 
+         Reservation.objects.create(
+             property=property,
+             start_date=start_date,
+             end_date=end_date,
+             number_of_nights=number_of_nights,
+             total_price=total_price,
+             guests=guests,
+             created_by=request.user
+         )
+ 
+         return JsonResponse({'success': True})
+     except Exception as e:
+         print('Error', e)
+ 
+         return JsonResponse({'success': False})
